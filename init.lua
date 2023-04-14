3b86213ad4e21d974f7e0b51d113f7f7b9ad014d
Maxime Bertonnier <m.bertonnier@gmail.com>
2023-04-15 00:54:47 +0200

    Fix eslint/prettier on ts files

--- init.lua
+++ init.lua
@@ -33,13 +33,13 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          "typescript",      -- let eslint do its magic
           -- "python",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "tsserver",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
