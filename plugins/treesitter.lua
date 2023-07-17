ffd676204b5f686b01c8f5fb9c0214f3cd4beaa8
Maxime Bertonnier <m.bertonnier@gmail.com>
2023-07-17 11:25:22 +0200

    Add terraform support

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "graphql", "prisma" },
+    ensure_installed = { "lua", "graphql", "prisma", "terraform" },
   },
 }
